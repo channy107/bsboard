357f996de8c58590eebb077bcb0bac5b8045a6ee
user <hunchan12@gmail.com>
2018-11-02 17:20:08 +0900

    홈페이지만들기, 게시판

--- board/models.py
+++ board/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+
 class Member(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     pw = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
+
+class boardDb(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=1000)
+
+
+
--- board/views.py
+++ board/views.py
@@ -1,14 +1,20 @@
-from django.http import JsonResponse
+from django.core.paginator import Paginator
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
+
+from board.form import BoardForm
 from .models import *
+import os
+from bsboard import settings
+
 
 def home(request):
-
     return render(request, 'board/home.html', {})
+
 
 def join(request):
     if request.method == 'GET':
-        return render(request,'board/join.html',{})
+        return render(request, 'board/join.html', {})
     else:
         id = request.POST['id']
         pw = request.POST['pw']
@@ -17,8 +23,9 @@ def join(request):
         m = Member(id=id, pw=pw, name=name)
         m.save()
 
-        return render(request,'board/join_result.html',
-                      {'id':id,'name':name})
+        return render(request, 'board/join_result.html',
+                      {'id': id, 'name': name})
+
 
 def id_check(request):
     id = request.POST['id']
@@ -27,11 +34,12 @@ def id_check(request):
     except Member.DoesNotExist as e:
         pass
 
-        res={'id':id, 'msg':'가입가능'}
+        res = {'id': id, 'msg': '가입가능'}
         return JsonResponse(res)
     else:
-        res={'id':id, 'msg':'가입불가'}
+        res = {'id': id, 'msg': '가입불가'}
         return JsonResponse(res)
+
 
 def login(request):
     if request.method == 'GET':
@@ -42,18 +50,53 @@ def login(request):
         pw = request.POST['pw']
 
         try:
-            Member.objects.get(id=id,pw=pw)
+            Member.objects.get(id=id, pw=pw)
         except Member.DoesNotExist:
             return redirect('login')
 
         else:
-            request.session['id']=id
+            request.session['id'] = id
             return redirect('/board/home')
 
-def board1(request):
 
-    return render(request, 'board/board1.html', {})
+def board1(request):
+    page = request.GET['page']
+    board_list = boardDb.objects.order_by('-id')
+    paginator = Paginator(board_list, 10)
+    page_info = paginator.page(page)
+    return render(request, 'board/board1.html',{'page_info':page_info})
+
+
+
 
 def writePage(request):
-
     return render(request, 'board/writePage.html', {})
+
+
+def upload1(request):
+    if request.method == 'GET':
+        return render(request, '', {})
+    else:
+        upload_file = request.FILES['my_file']
+
+        login_id = request.session['id']
+
+    try:
+        os.mkdir(login_id)
+    except FileExistsError:
+        pass
+
+    with open(login_id + '/' + upload_file.name,
+              'wb') as file:
+        for chunk in upload_file.chunks():
+            file.write(chunk)
+
+    return HttpResponse('완료' + upload_file.name)
+
+def insert(request):
+    for i in range(1, 1001):
+        boardDb.objects.create(title='제목'+ str(i), content = '내용'+str(i))
+
+    return HttpResponse('insert1000')
+
+
